5ae7f693302960f17c1e769fdb7c72d3b8cf18a7
RebeccaMiller <Rebecca.Miller@noaa.gov>
2017-11-30 13:48:44 -0800

    will this work

--- MBARI_GitHubCode.R
+++ MBARI_GitHubCode.R
@@ -2,4 +2,12 @@ getwd()
 list.files()  ##shows files and working directories
 setwd()  ##set working directory to something else
 
+##Git Tab, click on stage button to keep track, click on Commit to send to github, next click on "Push"
 
+##ggplot means grammar of graphics plot
+
+install.packages("tidyverse")
+library(tidyverse)
+
+##hiddendataset in R
+mpg
